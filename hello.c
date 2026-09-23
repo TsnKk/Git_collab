d121cced58fd9e72b7ce1c137b02c35122f37f06
Chanon <69010226@kmitl.ac.th>
2026-09-23 17:33:08 +0700

    add comment

--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+// Print a greeting message for the specified user
 int greeting(const char *name)
 {
     printf("Hello, %s!\n", name);
     return 0;
 }
+
 
 int main(void)
 {
